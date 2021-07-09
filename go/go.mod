d4ee5a2c528d493e0d2bc1a351e5d01bf5cd9212
Kentaro IMAJO <173752+imos@users.noreply.github.com>
2021-07-10 04:01:54 +0900

    Add server (#4)

---
+++ go/go.mod
@@ -0,0 +1,10 @@
+module github.com/imos/icfpc2021
+
+go 1.16
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/golang/glog v0.0.0-20210429001901-424d2337a529 // indirect
+	github.com/jmoiron/sqlx v1.3.4 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+)
---
+++ go/internal/handler/submit.go
@@ -0,0 +1,147 @@
+package handler
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/golang/glog"
+	"github.com/imos/icfpc2021/pkg/db"
+	"github.com/pkg/errors"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+func init() {
+	http.HandleFunc("/api/submit", handleAPISubmit)
+	http.HandleFunc("/submit", handleSubmit)
+}
+
+func handleSubmit(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	if r.Method == "GET" {
+		fmt.Fprintln(w, `
+<body><form action="/submit" method="POST">
+Problem ID: <input type="text" name="problem_id"><br><br>
+JSON:
+<textarea name="data"></textarea><br><br>
+<input type="submit" value="Submit">
+</form></body>
+`)
+		return
+	}
+	r.ParseForm()
+	problemID, err := strconv.ParseInt(r.Form.Get("problem_id"), 10, 64)
+	if err != nil {
+		fmt.Fprintf(w, "Failed to parse problem_id: %+v", err)
+	}
+	submissionID, err := Submit(ctx, problemID, r.Form.Get("data"))
+	if err != nil {
+		fmt.Fprintf(w, "Failed to submit: %+v", err)
+	}
+	fmt.Fprintf(w, "Submission ID: %d", submissionID)
+}
+
+func handleAPISubmit(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	if r.Body == nil {
+		glog.Errorf("body is empty")
+		w.WriteHeader(400)
+		return
+	}
+	defer r.Body.Close()
+	buf, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		glog.Errorf("body is broken: %#v", err)
+		w.WriteHeader(400)
+		return
+	}
+
+	problemID, err := strconv.ParseInt(
+		r.URL.Query().Get("problem_id"), 10, 64)
+	if err != nil {
+		glog.Errorf("Failed to parse problem ID: %+v", err)
+		w.WriteHeader(400)
+		return
+	}
+
+	poseID, err := Submit(ctx, problemID, string(buf))
+	if err != nil {
+		glog.Errorf("Failed to submit: %+v", err)
+		w.WriteHeader(400)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/plain")
+	fmt.Fprintf(w, "%d", poseID)
+}
+
+func Submit(ctx context.Context, problemID int64, solution string) (int64, error) {
+	poseID, err := submitToOfficial(problemID, solution)
+	if err != nil {
+		return 0, err
+	}
+
+	result, err := db.Execute(ctx,
+		"INSERT INTO submissions" +
+		"(problem_id, submission_data, submission_uuid, submission_submitted) " +
+		"VALUES(?, ?, ?, CURRENT_TIMESTAMP())",
+		problemID, solution, poseID)
+	if err != nil {
+		return 0, errors.Wrapf(err, "failed to insert a submission")
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, errors.Wrapf(err, "failed to get an insert ID")
+	}
+	return id, nil
+}
+
+func submitToOfficial(problemID int64, solution string) (string, error) {
+	glog.Infof("Problem ID: %d, solution: %s", problemID, solution)
+	var vertices struct {
+		Vertices [][]int64 `json:"vertices"`
+	}
+	if err := json.Unmarshal([]byte(solution), &vertices); err != nil {
+		return "", errors.Wrapf(err, "failed to parse the solution: %+v", err)
+	}
+	if len(vertices.Vertices) == 0 {
+		return "", errors.Errorf("No verticies are provided")
+	}
+
+	req, err := http.NewRequest(
+		"POST",
+		fmt.Sprintf("https://poses.live/api/problems/%d/solutions", problemID),
+		bytes.NewBuffer([]byte(solution)))
+	if err != nil {
+		return "", errors.Wrapf(err, "failed to create a submission request")
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer " + os.Getenv("UNAGI_API_KEY"))
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", errors.Wrapf(err, "failed to submit a solution")
+	}
+	defer resp.Body.Close()
+
+	buf, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", errors.Wrapf(err, "failed to read the submission response")
+	}
+
+	glog.Infof("Response: %s", string(buf))
+	var response struct {
+		ID string `json:"id"`
+	}
+	if err := json.Unmarshal(buf, &response); err != nil {
+		return "", errors.Wrapf(err, "failed to parse a response: %+v", err)
+	}
+	return response.ID, nil
+}
