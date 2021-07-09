ec881c567f547a804bebc9195903256ed6618c74
Toshiki Kataoka <tos.lunar@gmail.com>
2021-07-10 00:45:35 +0900

    load solution

--- gui/src/index.ts
+++ gui/src/index.ts
@@ -42,11 +42,11 @@ type Solution = {
 
 // 1.json
 // prettier-ignore
-let inputJson: Problem = {"hole":[[45,80],[35,95],[5,95],[35,50],[5,5],[35,5],[95,95],[65,95],[55,80]],"epsilon":150000,"figure":{"edges":[[2,5],[5,4],[4,1],[1,0],[0,8],[8,3],[3,7],[7,11],[11,13],[13,12],[12,18],[18,19],[19,14],[14,15],[15,17],[17,16],[16,10],[10,6],[6,2],[8,12],[7,9],[9,3],[8,9],[9,12],[13,9],[9,11],[4,8],[12,14],[5,10],[10,15]],"vertices":[[20,30],[20,40],[30,95],[40,15],[40,35],[40,65],[40,95],[45,5],[45,25],[50,15],[50,70],[55,5],[55,25],[60,15],[60,35],[60,65],[60,95],[70,95],[80,30],[80,40]]}};
+const sampleInput: Problem = {"hole":[[45,80],[35,95],[5,95],[35,50],[5,5],[35,5],[95,95],[65,95],[55,80]],"epsilon":150000,"figure":{"edges":[[2,5],[5,4],[4,1],[1,0],[0,8],[8,3],[3,7],[7,11],[11,13],[13,12],[12,18],[18,19],[19,14],[14,15],[15,17],[17,16],[16,10],[10,6],[6,2],[8,12],[7,9],[9,3],[8,9],[9,12],[13,9],[9,11],[4,8],[12,14],[5,10],[10,15]],"vertices":[[20,30],[20,40],[30,95],[40,15],[40,35],[40,65],[40,95],[45,5],[45,25],[50,15],[50,70],[55,5],[55,25],[60,15],[60,35],[60,65],[60,95],[70,95],[80,30],[80,40]]}};
 
 // solution in spec-v1.0.pdf
 // prettier-ignore
-const solutionJson: Solution = {"vertices": [[21, 28], [31, 28], [31, 87], [29, 41], [44, 43], [58, 70],[38, 79], [32, 31], [36, 50], [39, 40], [66, 77], [42, 29],[46, 49], [49, 38], [39, 57], [69, 66], [41, 70], [39, 60],[42, 25], [40, 35]]};
+const sampleOutput: Solution = {"vertices": [[21, 28], [31, 28], [31, 87], [29, 41], [44, 43], [58, 70],[38, 79], [32, 31], [36, 50], [39, 40], [66, 77], [42, 29],[46, 49], [49, 38], [39, 57], [69, 66], [41, 70], [39, 60],[42, 25], [40, 35]]};
 
 class ProblemRenderer {
   hole: Graphics;
@@ -122,19 +122,26 @@ class ProblemRenderer {
     }
   }
 
+  loadSolution(solutionJson: Solution): void {
+    if (this.vertices.length != solutionJson.vertices.length) {
+      alert("vertices.length differs");
+      return;
+    }
+    for (const [i, v] of solutionJson.vertices.entries()) {
+      this.vertices[i].position.set(...v);
+      this.updateVertex(i);
+    }
+  }
+
   render(c: Container): void {
     c.removeChildren();
     c.addChild(this.hole, ...this.edges.map(([, , g]) => g), ...this.vertices);
   }
 }
 
-function drawProblem() {
-  const r = new ProblemRenderer(inputJson);
-  r.render(mainContainer);
-}
-
-drawProblem();
-// loadSolution(solutionJson);
+let r = new ProblemRenderer(sampleInput);
+r.render(mainContainer);
+r.loadSolution(sampleOutput);
 
 const fileElem: any = document.getElementById("input-json")!;
 fileElem.addEventListener("change", () => {
@@ -143,7 +150,8 @@ fileElem.addEventListener("change", () => {
   const reader = new FileReader();
   reader.readAsText(file, "UTF-8");
   reader.onload = (e) => {
-    inputJson = JSON.parse(e.target!.result as string);
-    drawProblem();
+    const inputJson = JSON.parse(e.target!.result as string);
+    r = new ProblemRenderer(inputJson);
+    r.render(mainContainer);
   };
 });
