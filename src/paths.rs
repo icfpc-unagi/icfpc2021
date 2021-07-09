9808f811c82b7229e0d45fb5ddfdfb22b9c0dea7
Toshiki Kataoka <tos.lunar@gmail.com>
2021-07-10 08:48:10 +0900

    cargo fmt some files

--- src/paths.rs
+++ src/paths.rs
@@ -1,57 +1,77 @@
-use std::fmt::*;
-use svg::node::element::*;
-use std::io;
 use crate::*;
+use std::fmt::*;
+use std::io;
+use svg::node::element::*;
 
 pub fn polygon<T: std::fmt::Display>(points: &[P<T>]) -> String {
-  let mut s = String::new();
-  for P(x, y) in points {
-    write!(s, "{},{} ", x, y).unwrap();
-  }
-  s
+    let mut s = String::new();
+    for P(x, y) in points {
+        write!(s, "{},{} ", x, y).unwrap();
+    }
+    s
 }
 
 pub fn path<T: std::fmt::Display>(edges: &[(usize, usize)], vertices: &[P<T>]) -> String {
-  let mut s = String::new();
-  for &(i, j) in edges {
-    write!(
-      s,
-      "M{} {}L{} {}",
-      vertices[i].0, vertices[i].1, vertices[j].0, vertices[j].1
-    ).unwrap();
-  }
-  s
+    let mut s = String::new();
+    for &(i, j) in edges {
+        write!(
+            s,
+            "M{} {}L{} {}",
+            vertices[i].0, vertices[i].1, vertices[j].0, vertices[j].1
+        )
+        .unwrap();
+    }
+    s
 }
 
-
 pub fn render_problem_svg<W: io::Write>(prob: &Input, w: W) -> io::Result<()> {
-  let hole_polygon = paths::polygon(&prob.hole);
-  let figure_path = paths::path(&prob.figure.edges, &prob.figure.vertices);
+    let hole_polygon = paths::polygon(&prob.hole);
+    let figure_path = paths::path(&prob.figure.edges, &prob.figure.vertices);
 
-  let svg = svg::Document::new()
-    .set("height", 500).set("width", 500).set("viewBox", (0, 0, prob.hole.iter().map(|p| p.0).max().unwrap(), prob.hole.iter().map(|p| p.1).max().unwrap()))
-    .add(
-      Polygon::new()
-        .set("fill", "grey")
-        .set("points", hole_polygon),
-    )
-    .add(Path::new().set("stroke", "red").set("d", figure_path));
+    let svg = svg::Document::new()
+        .set("height", 500)
+        .set("width", 500)
+        .set(
+            "viewBox",
+            (
+                0,
+                0,
+                prob.hole.iter().map(|p| p.0).max().unwrap(),
+                prob.hole.iter().map(|p| p.1).max().unwrap(),
+            ),
+        )
+        .add(
+            Polygon::new()
+                .set("fill", "grey")
+                .set("points", hole_polygon),
+        )
+        .add(Path::new().set("stroke", "red").set("d", figure_path));
 
     svg::write(w, &svg)
 }
 
 pub fn render_pose_svg<W: io::Write>(prob: &Input, pose: &Output, w: W) -> io::Result<()> {
-  let hole_polygon = paths::polygon(&prob.hole);
-  let figure_path = paths::path(&prob.figure.edges, &pose.vertices);
+    let hole_polygon = paths::polygon(&prob.hole);
+    let figure_path = paths::path(&prob.figure.edges, &pose.vertices);
 
-  let svg = svg::Document::new()
-  .set("height", 500).set("width", 500).set("viewBox", (0, 0, prob.hole.iter().map(|p| p.0).max().unwrap(), prob.hole.iter().map(|p| p.1).max().unwrap()))
-  .add(
-      Polygon::new()
-        .set("fill", "grey")
-        .set("points", hole_polygon),
-    )
-    .add(Path::new().set("stroke", "red").set("d", figure_path));
+    let svg = svg::Document::new()
+        .set("height", 500)
+        .set("width", 500)
+        .set(
+            "viewBox",
+            (
+                0,
+                0,
+                prob.hole.iter().map(|p| p.0).max().unwrap(),
+                prob.hole.iter().map(|p| p.1).max().unwrap(),
+            ),
+        )
+        .add(
+            Polygon::new()
+                .set("fill", "grey")
+                .set("points", hole_polygon),
+        )
+        .add(Path::new().set("stroke", "red").set("d", figure_path));
 
-  svg::write(w, &svg)
+    svg::write(w, &svg)
 }
--- src/wasm.rs
+++ src/wasm.rs
@@ -8,41 +8,41 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
 extern "C" {
-  fn alert(s: &str);
+    fn alert(s: &str);
 }
 
 #[wasm_bindgen(start)]
 pub fn main() -> Result<(), JsValue> {
-  let window = web_sys::window().unwrap();
-  let document = window.document().unwrap();
-  let body = document.body().unwrap();
+    let window = web_sys::window().unwrap();
+    let document = window.document().unwrap();
+    let body = document.body().unwrap();
 
-  let svg = document.create_element_ns(Some("http://www.w3.org/2000/svg"), "svg")?;
-  svg.set_attribute("id", "_svg")?;
-  body.append_child(&svg)?;
+    let svg = document.create_element_ns(Some("http://www.w3.org/2000/svg"), "svg")?;
+    svg.set_attribute("id", "_svg")?;
+    body.append_child(&svg)?;
 
-  Ok(())
+    Ok(())
 }
 
 #[wasm_bindgen]
 pub fn render_problem(s: &str) -> Result<(), JsValue> {
-  let prob = serde_json::from_str::<Input>(s).unwrap();
-  let hole_polygon = paths::polygon(&prob.hole);
-  let figure_path = paths::path(&prob.figure.edges, &prob.figure.vertices);
+    let prob = serde_json::from_str::<Input>(s).unwrap();
+    let hole_polygon = paths::polygon(&prob.hole);
+    let figure_path = paths::path(&prob.figure.edges, &prob.figure.vertices);
 
-  let window = web_sys::window().unwrap();
-  let document = window.document().unwrap();
-  let svg = document.get_element_by_id("_svg").unwrap();
+    let window = web_sys::window().unwrap();
+    let document = window.document().unwrap();
+    let svg = document.get_element_by_id("_svg").unwrap();
 
-  let polygon = document.create_element_ns(Some("http://www.w3.org/2000/svg"), "polygon")?;
-  polygon.set_attribute("fill", "grey")?;
-  polygon.set_attribute("points", &hole_polygon)?;
-  svg.append_child(&polygon)?;
+    let polygon = document.create_element_ns(Some("http://www.w3.org/2000/svg"), "polygon")?;
+    polygon.set_attribute("fill", "grey")?;
+    polygon.set_attribute("points", &hole_polygon)?;
+    svg.append_child(&polygon)?;
 
-  let path = document.create_element_ns(Some("http://www.w3.org/2000/svg"), "path")?;
-  path.set_attribute("stroke", "red")?;
-  path.set_attribute("d", &figure_path)?;
-  svg.append_child(&path)?;
+    let path = document.create_element_ns(Some("http://www.w3.org/2000/svg"), "path")?;
+    path.set_attribute("stroke", "red")?;
+    path.set_attribute("d", &figure_path)?;
+    svg.append_child(&path)?;
 
-  Ok(())
+    Ok(())
 }
