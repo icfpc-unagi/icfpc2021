069b0cc5a01b9a23f53b3211b676fb6fba6c1a49
Kentaro IMAJO <173752+imos@users.noreply.github.com>
2021-07-10 18:09:16 +0900

    Add runner (#7)

--- go/go.mod
+++ go/go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/golang/glog v0.0.0-20210429001901-424d2337a529 // indirect
+	github.com/google/uuid v1.2.0 // indirect
 	github.com/jmoiron/sqlx v1.3.4 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 )
--- src/bin/calculate_score.rs
+++ src/bin/calculate_score.rs
@@ -1,32 +1,20 @@
 use icfpc2021::{*, util::*};
 use std::fs::File;
 
-fn compute_score(input: &Input, out: &Output) -> i64 {
-	let mut score = 0;
-    if input.figure.vertices.len() != out.vertices.len() {
-        return -1;
-    }
-	for &p in &input.hole {
-		let mut min = i64::max_value();
-		for q in &out.vertices {
-			min.setmin((p - q.to_owned()).abs2());
-		}
-		score += min;
-	}
-	score
-}
-
 fn main() -> std::io::Result<()> {
     let args: Vec<_> = std::env::args().collect();
     if args.len() < 3 {
       eprintln!("{} <problem.json> <pose.json>", args[0]);
       std::process::exit(1);
     }
-    let prob = serde_json::from_reader(File::open(&args[1])?)?;
-    let pose = serde_json::from_reader(File::open(&args[2])?)?;
-
+	let input = read_input_from_file(&std::path::PathBuf::from(&args[1]));
+	let output = read_output_from_file(&std::path::PathBuf::from(&args[2]));
   
-    let score = compute_score(&prob, &pose);
-    println!("{}", score);
+    let score = compute_score(&input, &output);
+    if score >= 1000000000 {
+        println!("-1");
+    } else {
+        println!("{}", score);
+    }
     Ok(())
 }
