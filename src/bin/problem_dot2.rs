c2593108705f4673d68ae5e44a87181fb554dd7d
Toshiki Kataoka <tos.lunar@gmail.com>
2021-07-10 15:54:29 +0900

    copy

---
+++ src/bin/problem_dot2.rs
@@ -0,0 +1,42 @@
+use std::io::Write;
+
+use icfpc2021::*;
+
+fn main() {
+    for entry in glob::glob("./problems/*.json").unwrap() {
+        let path = entry.unwrap();
+
+        let path = path.to_str().unwrap();
+        let filename = path
+            .split("/")
+            .collect::<Vec<_>>()
+            .last()
+            .unwrap()
+            .to_owned();
+        let problem_id: i64 = filename.split('.').collect::<Vec<_>>()[0].parse().unwrap();
+
+        let file = std::fs::File::open(&path).unwrap();
+        let reader = std::io::BufReader::new(file);
+        let input: Input = serde_json::from_reader(reader).unwrap();
+        // println!("{:?}", input.figure.edges);
+
+        let out_path = format!("./tmp/{}.dot", problem_id);
+        let out_file = std::fs::File::create(&out_path).unwrap();
+        let mut writer = std::io::BufWriter::new(out_file);
+        writer.write("graph{\n".as_bytes()).unwrap();
+        let vs = input.figure.vertices;
+        for i in 0..vs.len() {
+            writer
+                .write(format!("{} [shape=circle]\n", i).as_bytes())
+                .unwrap();
+        }
+        for (a, b) in input.figure.edges {
+            let d2 = (vs[a] - vs[b]).abs2();
+            let d = (d2 as f64).ln_1p() / 5.0 + 0.1;
+            writer
+                .write(format!("{} -- {} [len={}]\n", a, b, d).as_bytes())
+                .unwrap();
+        }
+        writer.write("}\n".as_bytes()).unwrap();
+    }
+}
