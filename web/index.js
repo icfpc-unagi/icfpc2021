c512d66a3267d449c000fbb69c609ce39cbd89df
sulume <sulume@gmail.com>
2021-07-11 20:27:48 +0900

    fix reference

--- web/index.js
+++ web/index.js
@@ -7,18 +7,21 @@ import * as wasm from "icfpc2021";
   let el_message = document.getElementById('_message')
   let el_morph = document.getElementById('_morph')
 
-  var problem = '';
-  var pose = ''
+  // TODO: class
+  var state = {
+    problem: '',
+    pose: '',
+  }
 
   async function render_for_hash(hash) {
     if (!hash.startsWith('#')) return
     let params = Object.fromEntries(hash.substr(1).split('&').map(e => e.split('=', 2).map(e => decodeURIComponent(e))))
     let p_problem = params['problem_url'] && fetch(params['problem_url']).then(resp => resp.ok && resp.text())
     let p_pose = params['pose_url'] && fetch(params['pose_url']).then(resp => resp.ok && resp.text())
-    problem = p_problem && await p_problem
-    pose = p_pose && await p_pose || problem && JSON.stringify({ vertices: JSON.parse(problem).figure.vertices })
-    render(problem, pose)
-    el_morph.disabled = !pose
+    state.problem = p_problem && await p_problem
+    state.pose = p_pose && await p_pose || state.problem && JSON.stringify({ vertices: JSON.parse(state.problem).figure.vertices })
+    render(state.problem, state.pose)
+    el_morph.disabled = !state.pose
   }
 
   function render(problem, pose) {
@@ -36,11 +39,11 @@ import * as wasm from "icfpc2021";
     document.querySelectorAll('circle[i]').forEach(el => {
       function f(dx, dy) {
         let i = parseInt(el.getAttribute('i'))
-        let json = JSON.parse(pose)
+        let json = JSON.parse(state.pose)
         json.vertices[i][0] += dx
         json.vertices[i][1] += dy
-        pose = JSON.stringify(json)
-        render(problem, pose)
+        state.pose = JSON.stringify(json)
+        render(state.problem, state.pose)
         document.querySelector(`circle[i="${i}"]`).focus()
       }
       el.addEventListener('keydown', ev => {
@@ -56,7 +59,7 @@ import * as wasm from "icfpc2021";
   }
 
   addEventListener('hashchange', () => render_for_hash(location.href), false)
-  el_problem_id.addEventListener('change', e => {
+  el_problem_id.addEventListener('change', () => {
     let newhash = `#problem_url=%2Fstatic%2Fproblems%2F${e.target.value}.json`
     history.replaceState(null, '', newhash)
     render_for_hash(newhash)
@@ -65,9 +68,8 @@ import * as wasm from "icfpc2021";
 
   el_pose.addEventListener('change', el => {
     try {
-      let validated = JSON.stringify(JSON.parse(el.target.value))
-      pose = validated
-      render(problem, pose)
+      state.pose = JSON.stringify(JSON.parse(el.target.value))
+      render(state.problem, state.pose)
       el.target.style.boxShadow = ''
     } catch (e) {
       console.error(e)
@@ -77,11 +79,11 @@ import * as wasm from "icfpc2021";
   })
 
   el_morph.addEventListener('click', async function () {
-    if (problem && pose && !this.disabled) {
+    if (state.problem && state.pose && !this.disabled) {
       this.disabled = true
       for (let i = 0; i < 10 && this.disabled; i++) {
-        pose = wasm.morph(problem, pose, 1000)
-        render(problem, pose)
+        state.pose = wasm.morph(state.problem, state.pose, 1000)
+        render(state.problem, state.pose)
         await new Promise(resolve => setTimeout(resolve, 10))
       }
       this.disabled = false
