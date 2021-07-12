a92abafa90e30429027d72f6012af47d38ad362c
sulume <sulume@gmail.com>
2021-07-12 17:54:41 +0900

    best_solution?obtained_bonus

--- go/internal/handler/best_solution.go
+++ go/internal/handler/best_solution.go
@@ -2,10 +2,11 @@ package handler
 
 import (
 	"fmt"
-	"github.com/golang/glog"
-	"github.com/imos/icfpc2021/pkg/db"
 	"net/http"
 	"strconv"
+
+	"github.com/golang/glog"
+	"github.com/imos/icfpc2021/pkg/db"
 )
 
 func init() {
@@ -14,20 +15,35 @@ func init() {
 
 func handleBestSolution(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
-	problemID, err := strconv.ParseInt(r.URL.Query().Get("problem_id"), 10, 64)
+	query := r.URL.Query()
+	problemID, err := strconv.ParseInt(query.Get("problem_id"), 10, 64)
+	obtained_bonus := query.Get("obtained_bonus")
 	if err != nil {
 		w.WriteHeader(404)
 		return
 	}
-	result, err := db.CellString(ctx, `
-SELECT submission_data FROM submissions NATURAL JOIN (
-    SELECT MIN(submission_id) AS submission_id FROM submissions NATURAL JOIN (
-        SELECT problem_id, MIN(submission_estimated_score) AS submission_estimated_score
-        FROM submissions WHERE problem_id = ? AND submission_estimated_score >= 0
-		GROUP BY problem_id
-    ) r
-) r
-`, problemID)
+	var result string
+	if len(obtained_bonus) == 0 {
+		result, err = db.CellString(ctx, `
+		SELECT submission_data FROM submissions NATURAL JOIN (
+				SELECT MIN(submission_id) AS submission_id FROM submissions NATURAL JOIN (
+						SELECT problem_id, MIN(submission_estimated_score) AS submission_estimated_score
+						FROM submissions WHERE problem_id = ? AND submission_estimated_score >= 0
+				GROUP BY problem_id
+				) r
+		) r
+		`, problemID)
+	} else {
+		result, err = db.CellString(ctx, `
+		SELECT submission_data FROM submissions NATURAL JOIN (
+				SELECT MIN(submission_id) AS submission_id FROM submissions NATURAL JOIN (
+						SELECT problem_id, MIN(submission_estimated_score) AS submission_estimated_score
+						FROM submissions WHERE problem_id = ? AND INSTR(submission_obtained_bonuses, ?) > 0 AND submission_estimated_score >= 0
+				GROUP BY problem_id
+				) r
+		) r
+		`, problemID, obtained_bonus)
+	}
 	if err != nil {
 		glog.Errorf("Failed to query: %+v", err)
 		w.WriteHeader(500)
